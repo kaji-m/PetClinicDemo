3b15b5cf82227d9676cf636809ddb56e0d034ae3
Kaji <58408331+kaji-m@users.noreply.github.com>
2020-09-30 21:41:38 +0900

    Update agent_download.sh

--- agent_download.sh
+++ agent_download.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+rm contrast.jar
 curl -X GET https://eval.contrastsecurity.com/Contrast/api/ng/442311fd-c9d6-44a9-a00b-2b03db2d816c/agents/default/JAVA -H 'Authorization: dHVyYm91QGkuc29mdGJhbmsuanA6TDBGUUZZN1JGRVBOQUI4RQ==' -H 'API-Key: EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u' -H 'Accept: application/json' -OJ
