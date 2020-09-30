c5f08d99e8069a4f20e230c0fbb36de0db105150
Kaji <58408331+kaji-m@users.noreply.github.com>
2020-09-30 21:54:39 +0900

    Update agent_download.sh

--- agent_download.sh
+++ agent_download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "remove ./contrast.jar"
 rm ./contrast.jar
-curl -X GET https://eval.contrastsecurity.com/Contrast/api/ng/c992a0ef-e965-4f92-a410-e09256a78758/agents/default/JAVA -H 'Authorization: dGFrYS5zaGlvemFraUBjb250cmFzdHNlY3VyaXR5LmNvbTpIM1YyMzdWVDdRN0xYQVg2' -H 'API-Key: sgHuezwDda34Fu6DJl4u7s2ZSzrn91SB' -H 'Accept: application/json' -OJ
+curl -X GET https://eval.contrastsecurity.com/Contrast/api/ng/442311fd-c9d6-44a9-a00b-2b03db2d816c/agents/default/JAVA -H 'Authorization: YXRzdWtvLm1hdHN1emF3YUBjb250cmFzdHNlY3VyaXR5LmNvbToxRTkxS0tYSUowUDRXWU9U' -H 'API-Key: EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u' -H 'Accept: application/json' -OJ
