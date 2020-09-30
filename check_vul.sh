7af75ac669f8a89cc86cef29bb4002ad215103e8
Kaji <58408331+kaji-m@users.noreply.github.com>
2020-09-30 23:12:19 +0900

    Update check_vul.sh

--- check_vul.sh
+++ check_vul.sh
@@ -4,7 +4,7 @@ APP_VERSION="CODEBUILD_TEST_$1"
 ORG_ID="442311fd-c9d6-44a9-a00b-2b03db2d816c"
 APP_ID="f17571b8-eb60-4767-81c7-230c6576fd47"
 count=`curl -s -X GET "https://eval.contrastsecurity.com/Contrast/api/ng/${ORG_ID}/traces/${APP_ID}/ids?appVersionTags=${APP_VERSION}&severities=CRITICAL" \
-        -H "Authorization: dHVyYm91QGkuc29mdGJhbmsuanA6TDBGUUZZN1JGRVBOQUI4RQ==" \
+        -H "Authorization: dHVyYm91QGkuc29mdGJhbmsuanA6MTJNSlRJNkVOMjQ0Wlk5Qw==" \
         -H "API-Key: EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u" \
         | jq '[.traces[]] | length'`
 echo $count
